8528a4c58fab5c41eb7bb2afce0f0f5d827b4f35
rubyyun0709-star <rubyyun0709@gmail.com>
2026-04-12 18:25:25 -0400

    Add Example component with location button

--- src/App.js
+++ src/App.js
@@ -23,3 +23,16 @@ function App() {
 }
 
 export default App;
+
+
+import { GrLocation } from "react-icons/gr";
+
+export default function Example() {
+  return (
+    <div style={{ display: "flex" }}>
+    	<button><GrLocation /></button>
+    	<p>Location</p>
+    </div>
+  );
+}
+
